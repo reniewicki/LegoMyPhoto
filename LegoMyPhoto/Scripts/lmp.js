3246bf005329f56237fd2e278c69242276aab35b
reniewicki <richardreniewicki@gmail.com>
2015-02-06 15:13:54 -0700

    Reboot

---
+++ LegoMyPhoto/Scripts/lmp.js
@@ -0,0 +1,19 @@
+﻿    $(function () {
+        
+        var getPage = function () {
+            var $a = $(this);
+
+            var options = {
+                url: $a.attr("href"),
+                type: "get"
+            };
+
+            $.ajax(options).done(function (data) {
+                var target = $a.parents("div.pagedList").attr("data-lmp-target");
+                $(target).replaceWith(data);
+            });
+        }
+
+        $(".main-content").on("click", ".pagedList a", getPage);
+    });
+
